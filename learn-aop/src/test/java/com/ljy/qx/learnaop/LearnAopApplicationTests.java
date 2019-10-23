5567860e3a67851d81b1e2ecf3cd57fd7026f2d2
X-ljy <17637938901@163.com>
2019-10-23 09:27:47 +0800

    AOP

---
+++ learn-aop/src/test/java/com/ljy/qx/learnaop/LearnAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljy.qx.learnaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
